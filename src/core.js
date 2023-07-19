e1f15964ad8d4eaebcbb0ca3dc082900650e982c
Marcin Jankowski <marcin.jankowski@talan.com>
2023-07-19 13:10:04 +0200

    solve task

--- src/core.js
+++ src/core.js
@@ -1,16 +1,25 @@
 // 1. Create a function named bakingTime that returns 50
+function bakingTime() {
+  return 50
+}
 
 // 2. Create a function named remainingBakeTime that accepts one parameter:
 // - the number of minutes your cake has been in the oven.
 //
 // It must return how many minutes your cake has left based on your parameter
 // and the result of the bakingTime function
+function remainingBakeTime(minutes) {
+    return bakingTime() - minutes
+}
 
 // 3. Create a function named calculatePreparationTime that accepts one parameter:
 // - the number of layers your cake has
 //
 // It must return how many minutes it will take to prepare your cake, based on
 // each layer taking 3 minutes to prepare
+function calculatePreparationTime(layers) {
+    return layers * 3
+}
 
 // 4. Create a function named totalTimeSpent that accepts two parameters:
 // - the number of layers your cake has
@@ -18,6 +27,9 @@
 // It must return how many minutes in total you have spent making the cake,
 // which is the sum of the preparation time and the number of minutes it's been in the oven.
 // Use your calculatePreparationTime function in the calculation.
+function totalTimeSpent(layers, minutes) {
+  return calculatePreparationTime(layers) + minutes
+}
 
 // Don't change the code below this line
 module.exports = {
--- src/extensions/extensions.js
+++ src/extensions/extensions.js
@@ -4,6 +4,15 @@
 // The function must return "Phil's cake is ready!" if the remaining minutes is 0,
 // "The cake is still baking!" if there are any remaining minutes left,
 // or "You didn't set a timer!" if no value is provided to the parameter
+function timerStatus(minutesLeft) {
+  if (minutesLeft === 0) {
+    return "Phil's cake is ready!"
+  } else if (minutesLeft > 0) {
+    return "The cake is still baking!"
+  } else if (minutesLeft === undefined) {
+    return "You didn't set a timer!"
+  } 
+}
 
 // 2. To help Phil prepare ahead of time, create a function named estimatePrepTime
 // that accepts two parameters:
@@ -13,6 +22,9 @@
 // number of ingredients provided and the prep time per ingredient.
 // If no prep time per ingredient is provided, the function should assume each ingredient
 // takes 2 minutes to prepare
+function estimatePrepTime(ingredients, prepTimePerIngredient = 2) {
+    return ingredients.length * prepTimePerIngredient
+}
 
 // 3. Phil needs to know the quantity of milk and eggs to use! Create a function
 // named calculateQuantities which accepts two parameters:
@@ -29,6 +41,12 @@
 //
 // calculateQuantities(["milk", "eggs"], 3)
 // returns: { sugar: 0, eggs: 6 }
+function calculateQuantities(ingredients, layers) {
+  return {
+    sugar: ingredients.includes("sugar") ? 100 * layers : 0,
+    eggs: ingredients.includes("eggs") ? 2 * layers: 0
+  }
+}
 
 // 4. Phil's cake is pretty small and only provides 1 portion. He wants to make a bigger one!
 // Create a function named improveRecipe that accepts two parameters:
@@ -42,6 +60,15 @@
 // Example:
 // improveRecipe({ eggs: 2, milk: 100, sugar: 200 }, 3)
 // returns: { eggs: 6, milk: 300, sugar: 600 }
+function improveRecipe(ingredients, portions) {
+  const updatedIngredients = {};
+
+  Object.entries(ingredients).forEach(([key, value]) => {
+    updatedIngredients[key] = value * portions;
+  });
+
+  return updatedIngredients;
+}
 
 // Don't change the code below this line
 module.exports = {
